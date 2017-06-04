c1f2b949b340cead0ffea31d050cbae3618d4fce
pragati4721 <pragati4721@gmail.com>
2017-06-04 11:00:56 +0530

    Completed the first challenge

--- Vector/Vector.py
+++ Vector/Vector.py
@@ -1,5 +1,40 @@
+import math
 class Vector(object):
     """docstring for Vector."""
-    def __init__(self, arg):
-        super(Vector, self).__init__()
-        self.arg = arg
+    def __init__(self,arg):
+        self.vec = arg
+    def add(self,arg):
+        if len(self.vec) != len(arg.vec):
+            print "Not possible"
+        else :
+            self.total = []
+            for i in range(0,len(self.vec)):
+                self.total.append(self.vec[i] + arg.vec[i])
+            print self.total
+    def subtract(self,arg):
+        if len(self.vec) != len(arg.vec):
+            print "Not possible"
+        else :
+            self.total = []
+            for i in range(0,len(self.vec)):
+                self.total.append(self.vec[i] - arg.vec[i])
+            print self.total
+    def norm(self):
+        self.total = 0
+        for i in range(0,len(self.vec)):
+            self.total+=self.vec[i]**2
+        print math.sqrt(self.total)
+    def dot(self,arg):
+        if len(self.vec) != len(arg.vec):
+            print "Not possible"
+        else :
+            self.total = 0
+            for i in range(0,len(self.vec)):
+                self.total+=(self.vec[i]*arg.vec[i])
+            print self.total
+a = Vector([7,5,3])
+b = Vector([4,5,6])
+c = a.add(b)
+b.norm()
+d = a.subtract(b)
+e = a.dot(b)
